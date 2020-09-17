bb8a8d480af61fb9c245935300284744821a8c30
RoiFuries <roi.furies@gmail.com>
2020-09-17 13:10:19 -0500

    ?

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import "../assets/styles/App.scss"
 import Header from '../components/Header'
+import SearchHeader from '../components/SearchHeader'
 import Footer from '../components/Footer'
 
 
@@ -8,6 +9,8 @@ import Footer from '../components/Footer'
 const App = () => (
     <div className="App">
         <Header/>
+        <SearchHeader/>
+       
         <h1>Contenido </h1>
         <Footer/>
     </div>
