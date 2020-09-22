990cfb27cb5a427f8f37e7d7cfe6277f2db43373
cesar puentes <caps2695@gmail.com>
2020-09-21 20:47:43 -0500

    resolve conflicts

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,19 +1,5 @@
 import React from "react"
 import "../assets/styles/App.scss"
-<<<<<<< HEAD
-import Header from '../components/Header'
-import HomeMain from '../components/HomeMain'
-import Footer from '../components/Footer'
-
-
-
-const Home = () => (
-    <div className="App">
-        <Header showSearch = {true} showSignInLogin = {true} />
-        <HomeMain/>
-        <Footer/>
-    </div>
-=======
 import Header from "../components/Header"
 import HomeMain from "../components/HomeMain"
 // import SearchHeader from '../components/SearchHeader'
@@ -23,6 +9,5 @@ const Home = () => (
     <Header showSearch={false} />
     <HomeMain />
   </>
->>>>>>> 6acbadffe040365cb942f91467e78f0876041e93
 )
 export default Home
--- src/routes/app.js
+++ src/routes/app.js
@@ -3,14 +3,17 @@ import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Home from "../containers/Home"
 import Login from "../containers/Login"
 import SignIn from "../containers/SignIn"
+import Layout from "../components/Layout"
 
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/signin" component={SignIn} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signin" component={SignIn} />
+      </Switch>
+    </Layout>
   </BrowserRouter>
 )
 export default App
