2ef43d0a50902c8cfe89e21cc92aa42e5fe23256
cesar puentes <caps2695@gmail.com>
2020-09-21 20:40:55 -0500

    resolve conflicts

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,37 +1,16 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from '../containers/Home';
-import Login from '../containers/Login';
-import SignIn from '../containers/SignIn';
-
-
-const App = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={ Home } />
-            <Route exact path="/login" component={ Login } />
-            <Route exact path="/signin" component={ SignIn } />
-        </Switch>
-    </BrowserRouter>
-);
-export default App
-=======
 import React from "react"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Home from "../containers/Home"
-import Layout from "../components/Layout"
 import Login from "../containers/Login"
+import SignIn from "../containers/SignIn"
 
 const App = () => (
   <BrowserRouter>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-    </Layout>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signin" component={SignIn} />
+    </Switch>
   </BrowserRouter>
 )
 export default App
->>>>>>> 6acbadffe040365cb942f91467e78f0876041e93
