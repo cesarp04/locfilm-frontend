5d75bb26f10acf5ec887788e8a3613994fb38d25
RoiFuries <roi.furies@gmail.com>
2020-09-23 21:57:24 -0500

    Search results page create

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import "../assets/styles/App.scss"
-<<<<<<< HEAD
 import Header from '../components/Header'
 import HomeMain from '../components/HomeMain'
 import Footer from '../components/Footer'
@@ -13,16 +12,5 @@ const Home = () => (
         <HomeMain/>
         <Footer/>
     </div>
-=======
-import Header from "../components/Header"
-import HomeMain from "../components/HomeMain"
-// import SearchHeader from '../components/SearchHeader'
-
-const Home = () => (
-  <>
-    <Header showSearch={false} />
-    <HomeMain />
-  </>
->>>>>>> 6acbadffe040365cb942f91467e78f0876041e93
 )
 export default Home
