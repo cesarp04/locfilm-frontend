9e1d279cfeff9f1727466034841ef3da2096a686
RoiFuries <roi.furies@gmail.com>
2020-09-24 22:47:09 -0500

    remove logo of footer and resolve something bugs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,9 @@
 import React from "react"
 import "../assets/styles/components/Footer.scss"
-import logo from "../assets/static/logoh.png"
 
 const Footer = () => (
   <footer>
     <section className="footerMain">
-      <figure className="footerMain__logo">
-        <a href="#">
-          <img src={logo} alt="Logo" />
-        </a>
-      </figure>
       <div className="footerMain__links">
         <a href="#">Privacy & Cookies Policy</a>
         <a href="#">Terms and Conditions</a>
