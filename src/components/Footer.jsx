e5bbc34818a3de43aba8dba24bba7b8af1fe23c5
cesar puentes <caps2695@gmail.com>
2020-09-30 23:12:00 -0500

    fixed conflicts and responsive design the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,57 +2,28 @@ import React from "react"
 import "../assets/styles/components/Footer.scss"
 
 const Footer = () => (
-
-  
   <footer>
-        <section className="footerMain">
-            <div className="footerMain__links">
-                <a href="#">Privacy & Cookies Policy</a>
-                <a href="#">Terms and Conditions</a>
-                <a href="#">Medidas Covid</a>
-            </div>
-            <div className="footerMain_conatinerNewslatter">
-                <div className="footerMain__Newsletter">
-                    <p>Newsletter</p>§
-                    <p>Sign up for the newsletter</p>
-                </div>
-                <div className="footerMain__BtnNewsletter">
-                    <input type="text" placeholder="Your Email"/>
-                    <a href="#">Sucribe</a>
-                </div>
-            </div>
-        </section>
-        <section className="footerCredits">
-            <p>2020. <a href="#">PLATZI MASTER</a></p>
-        </section>
-    </footer>
-  
-  
-  
-  
-  
-  // <footer>
-  //   <section classNameName="footerMain">
-  //     <div classNameName="footerMain__links">
-  //       <a href="#">Privacy & Cookies Policy</a>
-  //       <a href="#">Terms and Conditions</a>
-  //       <a href="#">Medidas Covid</a>
-  //     </div>
-  //     <div classNameName="footerMain__Newsletter">
-  //       <p>Newsletter</p>
-  //       <p>Sign up for the newsletter</p>
-  //     </div>
-  //     <div classNameName="footerMain__BtnNewsletter">
-  //       <input type="text" placeholder="Your Email" />
-  //       <a href="#">Sucribe</a>
-  //     </div>
-  //   </section>
-  //   <section classNameName="footerCredits">
-  //     <p>
-  //       2020. <a href="#">PLATZI MASTER</a>
-  //     </p>
-  //   </section>
-  // </footer>
+    <section className="footerMain">
+      <div className="footerMain__links">
+        <a href="#">Terms and Conditions</a>
+      </div>
+      <div className="footerMain_conatinerNewslatter">
+        <div className="footerMain__Newsletter">
+          <p>Newsletter</p>
+          <p>Sign up for the newsletter</p>
+        </div>
+        <div className="footerMain__BtnNewsletter">
+          <input type="text" placeholder="Your Email" />
+          <a href="#">Sucribe</a>
+        </div>
+      </div>
+    </section>
+    <section className="footerCredits">
+      <p>
+        2020. <a href="#">PLATZI MASTER</a>
+      </p>
+    </section>
+  </footer>
 )
 
 export default Footer
