4fd2223911c9fd171a22aaec687fbc188e47c3ae
RoiFuries <roi.furies@gmail.com>
2020-10-12 01:25:07 -0500

    update_myReservation_bugs

--- src/containers/MyReservations.jsx
+++ src/containers/MyReservations.jsx
@@ -12,21 +12,49 @@ function MyReservation() {
       <Header showSearch={true} showSignInLogin={true} />
       <section className="reservation">
         <h1 className="title-reservation">My Reservations</h1>
-        <div>
+        <div className="title-reservation--links">
           <Link className="upcoming">Upcoming</Link>
           <Link className="past">Past</Link>
           <Link className="canceled">Canceled</Link>
         </div>
+        
         <div className="container">
           <div className="card">
-            <figure>
-              <img src={IMAGEN_DEFAULT} alt="" />
+            <figure className="container__card__details__img">
+                <img src={IMAGEN_DEFAULT} alt="" />
             </figure>
+            <div className="container__card__details">
+              <div className="container__card__details--bts">
+                <button>Cancel</button>
+                <button>Rate</button>
+              </div>
+              <div className="container__card__details--dates">
+                <p>Start </p>
+                <p>End </p>
+              </div>
+              <div className="container__card__details--status">
+                <p>Status</p>
+              </div>
+            </div> 
           </div>
+
           <div className="card">
-            <figure>
-              <img src={IMAGEN_DEFAULT} alt="" />
+            <figure className="container__card__details__img">
+                <img src={IMAGEN_DEFAULT} alt="" />
             </figure>
+            <div className="container__card__details">
+              <div className="container__card__details--bts">
+                <button>Cancel</button>
+                <button>Rate</button>
+              </div>
+              <div className="container__card__details--dates">
+                <p>Start </p>
+                <p>End </p>
+              </div>
+              <div className="container__card__details--status">
+                <p>Status</p>
+              </div>
+            </div> 
           </div>
         </div>
       </section>
