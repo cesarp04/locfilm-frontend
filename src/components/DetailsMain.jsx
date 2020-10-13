d94cb94aa85e8d26707d3ab1c49f36919f5ae6f0
cesar puentes <caps2695@gmail.com>
2020-10-12 23:26:24 -0500

    delete images defaults

--- src/components/DetailsMain.jsx
+++ src/components/DetailsMain.jsx
@@ -1,155 +1,155 @@
-import React, { useState } from 'react';
+import React, { useState } from "react"
 import "../assets/styles/components/Details.scss"
 import BasecampIcon from "../assets/static/i_camp.png"
 import DressRoomIcon from "../assets/static/i_dress.png"
 import ParkingIcon from "../assets/static/i_parking.png"
 import WC from "../assets/static/i_bath.png"
 import LivingRoom from "../assets/static/i_livingroom.png"
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css'
-import Cover from "../assets/static/cover-centlali.jpg"
-import Cross1 from "../assets/static/cross1.jpg"
-import Cross2 from "../assets/static/cross2.jpg"
-import Cross3 from "../assets/static/cross3.jpg"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
 
+const DetailsMain = ({ location }) => {
+  const [startDate, setStartDate] = useState(new Date())
+  function testStart(fecha) {
+    console.log(fecha)
+    setStartDate(fecha)
+  }
 
+  const [endDate, setendDate] = useState(new Date())
+  function testEnd(fecha) {
+    console.log(fecha)
+    setendDate(fecha)
+  }
 
-const DetailsMain = ({location}) => {
+  //
+  const [showFilterModal, setShowFilterModal] = useState(false)
+  function changeModalVisibility() {
+    setShowFilterModal(!showFilterModal)
+    console.log(showFilterModal)
+  }
 
-        const [startDate, setStartDate] = useState(new Date());
-        function testStart (fecha){
-            console.log(fecha)
-            setStartDate(fecha)
-        }
-
-        const [endDate, setendDate] = useState(new Date());
-        function testEnd (fecha){
-            console.log(fecha)
-            setendDate(fecha)
-        }
-
-        // 
-        const [showFilterModal, setShowFilterModal] = useState(false)
-        function changeModalVisibility () {
-            setShowFilterModal (!showFilterModal)
-            console.log(showFilterModal)
-        }
-
-    return(
-        
-      <main className="Container">
-        {/* <h1>{location.name}</h1> */}
-        <figure className="Container__cover">
-            <img src={location.main_image? location.main_image: Cover} alt="Cover_Crossfit"/>
-        </figure>
-        <section className="Container__info">
-            <div className="Container__about">
-                <h2>{location.name}</h2>
-                <p>Direccion: {location.address}, Ciudad:{location.city}</p>
+  return (
+    <main className="Container">
+      {/* <h1>{location.name}</h1> */}
+      <figure className="Container__cover">
+        <img src={location.main_image} alt="no images available" />
+      </figure>
+      <section className="Container__info">
+        <div className="Container__about">
+          <h2>{location.name}</h2>
+          <p>
+            Direccion: {location.address}, Ciudad:{location.city}
+          </p>
+        </div>
+        <div className="Container__services">
+          <h2>Description</h2>
+          <p>{location.description}</p>
+          <div className="Container__services--icons">
+            {location.has_cattering ? (
+              <figure>
+                <img src={BasecampIcon} alt="Icon Basecamp" />
+                <p>Basecamp area</p>
+              </figure>
+            ) : null}
+            {location.has_dressing_room ? (
+              <figure>
+                <img src={DressRoomIcon} alt="Icon Dressing Room" />
+                <p>Dressing rooms</p>
+              </figure>
+            ) : null}
+            {location.hasParking ? (
+              <figure>
+                <img src={ParkingIcon} alt="Icon Parking" />
+                <p>Parking</p>
+              </figure>
+            ) : null}
+            {location.has_bathroom ? (
+              <figure>
+                <img src={WC} alt="Icon W.C." />
+                <p>W.C.</p>
+              </figure>
+            ) : null}
+            <figure>
+              <img src={LivingRoom} alt="Icon Living Room" />
+              <p>Living Room</p>
+            </figure>
+          </div>
+        </div>
+      </section>
+      <section className="Container__datesAndSlider">
+        <div className="Container__datesAndSlider__dates">
+          <div className="Container__datesAndSlider__dates--checkbox">
+            <input
+              type="radio"
+              name=""
+              className="cbox1"
+              id="cbox1"
+              value="checkbos"
+            />
+            <label>Reserver Location</label>
+          </div>
+          <div className="Container__datesAndSlider__dates--choose">
+            <p>Choose the days to film</p>
+            <div className="Container__datesAndSlider__dates--choose--btns">
+              <div className="Container__datesAndSlider__dates--choose--datePicker">
+                <DatePicker
+                  className="Container__datesAndSlider__dates--choose--datePicker1"
+                  selected={startDate}
+                  onChange={(date) => testStart(date)}
+                  selectsStart
+                  startDate={startDate}
+                  endDate={endDate}
+                />
+              </div>
+              <div className="Container__datesAndSlider__dates--choose--inputs">
+                <DatePicker
+                  className="Container__datesAndSlider__dates--choose--datePicker1"
+                  selected={endDate}
+                  onChange={(date) => testEnd(date)}
+                  selectsEnd
+                  startDate={startDate}
+                  endDate={endDate}
+                  minDate={startDate}
+                />
+              </div>
             </div>
-            <div className="Container__services">
-                <h2>Description</h2>
-                <p>
-                   {location.description}
-                </p>
-                <div className="Container__services--icons">
-                    {location.has_cattering?
-                        <figure>
-                            <img src={BasecampIcon} alt="Icon Basecamp"/>
-                            <p>Basecamp area</p>
-                        </figure>
-                    :null}
-                    {location.has_dressing_room?
-                        <figure>
-                            <img src={DressRoomIcon} alt="Icon Dressing Room"/>
-                            <p>Dressing rooms</p>
-                        </figure>
-                    :null}
-                    {location.hasParking?
-                        <figure>
-                            <img src={ParkingIcon} alt="Icon Parking"/>
-                            <p>Parking</p>
-                        </figure>
-                    :null}
-                    {location.has_bathroom?
-                        <figure>
-                            <img src={WC} alt="Icon W.C."/>
-                            <p>W.C.</p>
-                        </figure>
-                    :null}
-                    <figure>
-                        <img src={LivingRoom} alt="Icon Living Room"/>
-                        <p>Living Room</p>
-                    </figure>
-                </div>
+            <div className="Container__datesAndSlider__dates--choose--btnSend">
+              <div href="#" className="Container__datesAndSlider__dates--btn">
+                <a href="#">Check! </a>
+              </div>
             </div>
-        </section>
-        <section className="Container__datesAndSlider">
-            <div className="Container__datesAndSlider__dates">
-                <div className="Container__datesAndSlider__dates--checkbox">
-                    <input type="radio" name="" className="cbox1" id="cbox1" value="checkbos"/>
-                    <label>Reserver Location</label>
-                </div>
-                <div className="Container__datesAndSlider__dates--choose">
-                    <p>
-                        Choose the days to film
-                    </p>
-                    <div className="Container__datesAndSlider__dates--choose--btns">
-                        <div className="Container__datesAndSlider__dates--choose--datePicker">
-                            <DatePicker className="Container__datesAndSlider__dates--choose--datePicker1" selected={startDate} onChange={date => testStart(date)}
-                            selectsStart
-                            startDate={startDate}
-                            endDate={endDate} 
-                            />
-                        </div>
-                        <div className="Container__datesAndSlider__dates--choose--inputs">
-                            <DatePicker className="Container__datesAndSlider__dates--choose--datePicker1" selected={endDate} onChange={date => testEnd(date)}
-                            selectsEnd
-                            startDate={startDate}
-                            endDate={endDate}
-                            minDate={startDate}
-                            />                        
-                       </div>
-                    </div>
-                    <div className="Container__datesAndSlider__dates--choose--btnSend">
-                        <div href="#" className="Container__datesAndSlider__dates--btn"> 
-                            <a href="#">Check! </a> 
-                        </div>
-                    </div>
-                </div>
+          </div>
+        </div>
+        <div className="Container__datesAndSlider__slider">
+          <input type="radio" id="1" name="image-slide" hidden />
+          <input type="radio" id="2" name="image-slide" hidden />
+          <input type="radio" id="3" name="image-slide" hidden />
+          <div className="slide">
+            <div className="item-slide">
+              <img src="" alt="no images available" />
             </div>
-            <div className="Container__datesAndSlider__slider">
-                <input type="radio" id="1" name="image-slide" hidden/>
-                <input type="radio" id="2" name="image-slide" hidden/>
-                <input type="radio" id="3" name="image-slide" hidden/>
-                <div className="slide">
-                    <div className="item-slide">
-                        <img src={Cross1}/>
-                    </div>
-                    <div className="item-slide">
-                        <img src={Cross2}/>
-                    </div>
-                    <div className="item-slide">
-                        <img src={Cross3}/>
-                    </div>
-                </div>
-                <div className="pagination">
-                    <label className="pagination-item" htmlFor="1">
-                        <img src={Cross1}/>
-                    </label>
-                    <label className="pagination-item" htmlFor="2">
-                        <img src={Cross2}/>
-                    </label>
-                    <label className="pagination-item" htmlFor="3">
-                        <img src={Cross3}/>
-                    </label>
-                </div>
-            </div>            
-        </section>
+            <div className="item-slide">
+              <img src="" alt="no images available" />
+            </div>
+            <div className="item-slide">
+              <img src="" alt="no images available" />
+            </div>
+          </div>
+          <div className="pagination">
+            <label className="pagination-item" htmlFor="1">
+              <img src="" alt="no images available" />
+            </label>
+            <label className="pagination-item" htmlFor="2">
+              <img src="" alt="no images available" />
+            </label>
+            <label className="pagination-item" htmlFor="3">
+              <img src="" alt="no images available" />
+            </label>
+          </div>
+        </div>
+      </section>
     </main>
-  
   )
 }
-  
-  export default DetailsMain
-  
+
+export default DetailsMain
--- src/containers/MyReservations.jsx
+++ src/containers/MyReservations.jsx
@@ -1,40 +1,38 @@
 import React, { useEffect, useState } from "react"
 import Header from "../components/Header"
 import "../assets/styles/components/MyReservations.scss"
-import { getMyReservations } from '../actions/reservationActions'
-
-import IMAGEN_DEFAULT from "../assets/static/house_default.jpg"
+import { getMyReservations } from "../actions/reservationActions"
 
 function MyReservation() {
-
-  const [reservations, setReservations] = useState([]);
-  const [loaded, setLoaded] = useState(false);
-  const [hasData, setHasData] = useState(false);
+  const [reservations, setReservations] = useState([])
+  const [loaded, setLoaded] = useState(false)
+  const [hasData, setHasData] = useState(false)
 
   const getUpcoming = () => {
-    return reservations.filter(r => r.status === 'Confirmed' || r.status === 'Pending');
+    return reservations.filter(
+      (r) => r.status === "Confirmed" || r.status === "Pending"
+    )
   }
 
   const getPast = () => {
-    return reservations.filter(r => r.status === 'Finished');
+    return reservations.filter((r) => r.status === "Finished")
   }
 
   const getcanceled = () => {
-    return reservations.filter(r => r.status === 'Cancelled');
+    return reservations.filter((r) => r.status === "Cancelled")
   }
 
   const showReservation = (type) => {
-
     switch (type) {
       case "Upcoming":
-        setReservationList(getUpcoming());
-        break;
+        setReservationList(getUpcoming())
+        break
       case "Past":
-        setReservationList(getPast());
-        break;
+        setReservationList(getPast())
+        break
       case "Cancelled":
-        setReservationList(getcanceled());
-        break;
+        setReservationList(getcanceled())
+        break
     }
   }
 
@@ -43,31 +41,34 @@ function MyReservation() {
   }
 
   const ratingReservation = (id) => {
-
     let rating = {
       accesibility: 5,
       conditions: 5,
       average: 5,
-      description: 'great location'
+      description: "great location",
     }
     console.log("Calificar: ", id)
   }
 
-  const [reservationList, setReservationList] = useState([]);
+  const [reservationList, setReservationList] = useState([])
 
   useEffect(() => {
     getMyReservations()
       .then((response) => {
-        setReservations(response.data);
-        setReservationList(response.data.filter(r => r.status === 'Confirmed' || r.status === 'Pending'));
-        setHasData(response.data.length > 0);
-        setLoaded(true);
+        setReservations(response.data)
+        setReservationList(
+          response.data.filter(
+            (r) => r.status === "Confirmed" || r.status === "Pending"
+          )
+        )
+        setHasData(response.data.length > 0)
+        setLoaded(true)
       })
       .catch((response) => {
-        setHasData(false);
-        setLoaded(true);
+        setHasData(false)
+        setLoaded(true)
       })
-  }, [setHasData, setLoaded, setReservations, setReservationList]);
+  }, [setHasData, setLoaded, setReservations, setReservationList])
 
   return (
     <>
@@ -76,57 +77,72 @@ function MyReservation() {
         <h1 className="title-reservation">My Reservations</h1>
 
         <div>
-          <a className="upcoming" onClick={() => showReservation('Upcoming')}>Upcoming</a>
-          <a className="past" onClick={() => showReservation('Past')}>Past</a>
-          <a className="canceled" onClick={() => showReservation('Cancelled')}>Canceled</a>
-
+          <a className="upcoming" onClick={() => showReservation("Upcoming")}>
+            Upcoming
+          </a>
+          <a className="past" onClick={() => showReservation("Past")}>
+            Past
+          </a>
+          <a className="canceled" onClick={() => showReservation("Cancelled")}>
+            Canceled
+          </a>
         </div>
-        
+
         <div className="container">
-
-
-          {!loaded?<h1>Loding data</h1>
-            : hasData ? reservationList.map((reservation, index) =>
-              (<div key={index} className="card">
-                <figure >
+          {!loaded ? (
+            <h1>Loding data</h1>
+          ) : hasData ? (
+            reservationList.map((reservation, index) => (
+              <div key={index} className="card">
+                <figure>
                   <img src={reservation.location_id.main_image} alt=" " />
                 </figure>
                 <div className="card__info">
-                <h3>{reservation.location_id.name}</h3>
+                  <h3>{reservation.location_id.name}</h3>
                   <p> {reservation.location_id.city}</p>
                   <p>Status: {reservation.status}</p>
-                  {(reservation.status === 'Pending') ?
-                    <button className="card--btns" onClick={() => cancelReservation(reservation.id)}>Cancel</button>
-                    : null}
-                  {(reservation.status === 'Finished') ?
-                    <button className="card--btns" onClick={() => ratingReservation(reservation.id)}>Rating</button>
-                    : null}
+                  {reservation.status === "Pending" ? (
+                    <button
+                      className="card--btns"
+                      onClick={() => cancelReservation(reservation.id)}
+                    >
+                      Cancel
+                    </button>
+                  ) : null}
+                  {reservation.status === "Finished" ? (
+                    <button
+                      className="card--btns"
+                      onClick={() => ratingReservation(reservation.id)}
+                    >
+                      Rating
+                    </button>
+                  ) : null}
                 </div>
-                
-              </div>)) : <h1>No data</h1> 
-          }
-
+              </div>
+            ))
+          ) : (
+            <h1>No data</h1>
+          )}
         </div>
       </section>
       <section className="modal-star">
         <div className="modal-star__container">
-          
           <div className="modal-star__container__accessibility">
-            <p className="modal-star__container--titles" >Accessibility</p>
+            <p className="modal-star__container--titles">Accessibility</p>
             <div className="modal-star__container__accessibility--range">
-              <input id="radio1" type="radio" name="estrellas" value="5"/>
+              <input id="radio1" type="radio" name="estrellas" value="5" />
               <label for="radio1">★</label>
 
-              <input id="radio2" type="radio" name="estrellas" value="4"/>
+              <input id="radio2" type="radio" name="estrellas" value="4" />
               <label for="radio2">★</label>
 
-              <input id="radio3" type="radio" name="estrellas" value="3"/>
+              <input id="radio3" type="radio" name="estrellas" value="3" />
               <label for="radio3">★</label>
 
-              <input id="radio4" type="radio" name="estrellas" value="2"/>
+              <input id="radio4" type="radio" name="estrellas" value="2" />
               <label for="radio4">★</label>
 
-              <input id="radio5" type="radio" name="estrellas" value="1"/>
+              <input id="radio5" type="radio" name="estrellas" value="1" />
               <label for="radio5">★</label>
             </div>
           </div>
@@ -134,56 +150,57 @@ function MyReservation() {
           <div className="modal-star__container__Quality">
             <p className="modal-star__container--titles">Quality</p>
             <div className="modal-star__container__accessibility--range">
-              <input id="radio1" type="radio" name="estrellas" value="5"/>
+              <input id="radio1" type="radio" name="estrellas" value="5" />
               <label for="radio1">★</label>
 
-              <input id="radio2" type="radio" name="estrellas" value="4"/>
+              <input id="radio2" type="radio" name="estrellas" value="4" />
               <label for="radio2">★</label>
 
-              <input id="radio3" type="radio" name="estrellas" value="3"/>
+              <input id="radio3" type="radio" name="estrellas" value="3" />
               <label for="radio3">★</label>
 
-              <input id="radio4" type="radio" name="estrellas" value="2"/>
+              <input id="radio4" type="radio" name="estrellas" value="2" />
               <label for="radio4">★</label>
-              
-              <input id="radio5" type="radio" name="estrellas" value="1"/>
+
+              <input id="radio5" type="radio" name="estrellas" value="1" />
               <label for="radio5">★</label>
             </div>
           </div>
-          
+
           <div className="modal-star__container__Quality">
             <p className="modal-star__container--titles">Price</p>
             <div className="modal-star__container__accessibility--range">
-              <input id="radio1" type="radio" name="estrellas" value="5"/>
+              <input id="radio1" type="radio" name="estrellas" value="5" />
               <label for="radio1">★</label>
 
-              <input id="radio2" type="radio" name="estrellas" value="4"/>
+              <input id="radio2" type="radio" name="estrellas" value="4" />
               <label for="radio2">★</label>
 
-              <input id="radio3" type="radio" name="estrellas" value="3"/>
+              <input id="radio3" type="radio" name="estrellas" value="3" />
               <label for="radio3">★</label>
 
-              <input id="radio4" type="radio" name="estrellas" value="2"/>
+              <input id="radio4" type="radio" name="estrellas" value="2" />
               <label for="radio4">★</label>
-              
-              <input id="radio5" type="radio" name="estrellas" value="1"/>
+
+              <input id="radio5" type="radio" name="estrellas" value="1" />
               <label for="radio5">★</label>
             </div>
-            <button className="modal-star__container__accessibility--btn"> Send</button>
-          </div>        
-         
+            <button className="modal-star__container__accessibility--btn">
+              {" "}
+              Send
+            </button>
+          </div>
         </div>
       </section>
       <section className="modal-cancel">
-            <div className="modal-cancel--container">
-              <h2>Dude! Wait!</h2>
-              <p>Are you sure you want to cancel your reservation?</p>
-              <div className="modal-cancel--container-btns">
-                  <button>No!</button>
-                  <button>Yes!</button>
-              </div>
-            </div>
-            
+        <div className="modal-cancel--container">
+          <h2>Dude! Wait!</h2>
+          <p>Are you sure you want to cancel your reservation?</p>
+          <div className="modal-cancel--container-btns">
+            <button>No!</button>
+            <button>Yes!</button>
+          </div>
+        </div>
       </section>
     </>
   )
