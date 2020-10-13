cf7d576397c208ca5f71e3c46a7316bbb6f241fd
cesar puentes <caps2695@gmail.com>
2020-10-13 09:58:20 -0500

    Image carousel fixes detail page

--- src/containers/Details.jsx
+++ src/containers/Details.jsx
@@ -3,67 +3,64 @@ import "../assets/styles/App.scss"
 
 import Header from "../components/Header"
 import DetailsMain from "../components/DetailsMain"
-import { useParams } from 'react-router-dom'
-import { getLocationById, getImagesLocation } from '../actions/locationActions'
+import { useParams } from "react-router-dom"
+import { getLocationById, getImagesLocation } from "../actions/locationActions"
 
 const Details = () => {
-
   // const [location, setLocation]= useState(null);
-  const { id } = useParams();
-  const [loaded, setLoaded] = useState(false);
-  const [error, setError] = useState(false);
-  const [location, setLocation] = useState();
+  const { id } = useParams()
+  const [loaded, setLoaded] = useState(false)
+  const [error, setError] = useState(false)
+  const [location, setLocation] = useState()
 
-  let view = null;
+  let view = null
 
   useEffect(() => {
     getLocationById(id)
       .then((l) => {
-
         getImagesLocation(l.data.id)
-          .then((response)=>{
-            l.data.images = response.data;
-            setLocation(l.data);
-            setLoaded(true);
-            setError(false);
+          .then((response) => {
+            l.data.images = response.data
+            setLocation(l.data)
+            setLoaded(true)
+            setError(false)
           })
-          .catch((response)=>{
-            l.data.images = [];
-            setLocation(l.data);
-            setLoaded(true);
-            setError(false);
-          });
+          .catch((response) => {
+            l.data.images = []
+            setLocation(l.data)
+            setLoaded(true)
+            setError(false)
+          })
       })
       .catch((e) => {
-        setLoaded(false);
-        setError(true);
-      });
-  }, [setLocation,setLoaded,setError]);
-
+        setLoaded(false)
+        setError(true)
+      })
+  }, [setLocation, setLoaded, setError])
 
   if (loaded) {
     view = (
       <>
-        <Header showSearch={false} showSignInLogin={true} />
+        <Header showSearch={false} />
         <DetailsMain location={location} />
       </>
-    );
+    )
   } else if (!error) {
     view = (
       <>
-        <Header showSearch={false} showSignInLogin={true} />
+        <Header showSearch={false} />
         <h1>Loading location {id}</h1>
       </>
-    );
+    )
   } else {
     view = (
       <>
-        <Header showSearch={false} showSignInLogin={true} />
+        <Header showSearch={false} />
         <h1>No se encontró la locación</h1>
       </>
-    );
+    )
   }
 
-  return view;
+  return view
 }
 export default Details
